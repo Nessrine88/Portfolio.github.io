4368283b193037e1bf469de380ed224ac518a5ba
Nessrine88 <macherki.nessrine@yahoo.com>
2023-06-27 00:59:42 +0100

    fix js linters

--- main.js
+++ main.js
@@ -1,41 +1,39 @@
+// Get necessary elements
+const menuIcon = document.getElementById('menuIcon');
+const closeIcon = document.getElementById('closeIcon');
+const menuList = document.getElementById('menuList');
 
-  // Get necessary elements
-  var menuIcon = document.getElementById("menuIcon");
-  var closeIcon = document.getElementById("closeIcon");
-  var menuList = document.getElementById("menuList");
+// Add event listener to the menu icon
+menuIcon.addEventListener('click', () => {
+  menuIcon.style.display = 'none';
+  closeIcon.style.display = 'inline-block';
+  menuList.classList.add('open');
+});
 
-  // Add event listener to the menu icon
-  menuIcon.addEventListener("click", function() {
-    menuIcon.style.display = "none";
-    closeIcon.style.display = "inline-block";
-    menuList.classList.add("open");
-  });
+// Add event listener to the close icon
+closeIcon.addEventListener('click', () => {
+  menuIcon.style.display = 'inline-block';
+  closeIcon.style.display = 'none';
+  menuList.classList.remove('open');
+});
 
-  // Add event listener to the close icon
-  closeIcon.addEventListener("click", function() {
-    menuIcon.style.display = "inline-block";
-    closeIcon.style.display = "none";
-    menuList.classList.remove("open");
-  });
+// Add event listeners to the menu options
+const menuOptions = menuList.getElementsByTagName('a');
+for (let i = 0; i < menuOptions.length; i += 1) {
+  menuOptions[i].addEventListener('click', function menuOption(e) {
+    e.preventDefault(); // Prevent the default link behavior
 
-  // Add event listeners to the menu options
-  var menuOptions = menuList.getElementsByTagName("a");
-  for (var i = 0; i < menuOptions.length; i++) {
-    menuOptions[i].addEventListener("click", function(e) {
-      e.preventDefault(); // Prevent the default link behavior
+    const target = this.getAttribute('href'); // Get the target section ID
+    const targetSection = document.querySelector(target); // Find the target section
 
-      var target = this.getAttribute("href"); // Get the target section ID
-      var targetSection = document.querySelector(target); // Find the target section
-
-      // Scroll to the target section smoothly
-      targetSection.scrollIntoView({
-        behavior: "auto"
-      });
-
-      // Close the menu
-      menuIcon.style.display = "inline-block";
-      closeIcon.style.display = "none";
-      menuList.classList.remove("open");
+    // Scroll to the target section smoothly
+    targetSection.scrollIntoView({
+      behavior: 'auto',
     });
-  }
 
+    // Close the menu
+    menuIcon.style.display = 'inline-block';
+    closeIcon.style.display = 'none';
+    menuList.classList.remove('open');
+  });
+}
